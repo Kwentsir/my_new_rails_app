5d74327433c30d71fcefd686422af9f18dd12fd7
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-28 17:02:41 +0000

    Set up project and linters

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,16 @@
+class RootController < ApplicationController
+  def indexrails
+  end
+
+  def g
+  end
+
+  def controller
+  end
+
+  def Root
+  end
+
+  def index
+  end
+end
---
+++ test/controllers/root_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class RootControllerTest < ActionDispatch::IntegrationTest
+  test "should get indexrails" do
+    get root_indexrails_url
+    assert_response :success
+  end
+
+  test "should get g" do
+    get root_g_url
+    assert_response :success
+  end
+
+  test "should get controller" do
+    get root_controller_url
+    assert_response :success
+  end
+
+  test "should get Root" do
+    get root_Root_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get root_index_url
+    assert_response :success
+  end
+end
