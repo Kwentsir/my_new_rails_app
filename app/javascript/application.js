e2661a6d487d92fe95a6be525087a00e75da6be3
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-30 17:09:27 +0000

    Create react and rails app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,17 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import "./controllers";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./components/redux/configureStore";
 
-function App() {
-  return <h1>Hello World!</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
